10ae9f8ab27c37731fb9b190888caf3e7ed7d274
Roger Silva <rogersilvati@gmail.com>
2021-10-05 00:52:08 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,9 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller obj2 = new Seller(1, "Roger Silva", "roger.silva@email.com", new Date(), 3000.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(obj);
         System.out.println(obj2);
     }
