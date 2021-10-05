681bb41c7edbf8a0e18b5203026e0cbeb19d951d
Roger Silva <rogersilvati@gmail.com>
2021-10-05 01:33:37 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,20 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        Seller obj2 = new Seller(1, "Roger Silva", "roger.silva@email.com", new Date(), 3000.00, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
-        System.out.println(obj);
-        System.out.println(obj2);
+        System.out.println(seller);
     }
 }
