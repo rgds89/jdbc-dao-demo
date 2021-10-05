5f4e972add00a99ed7d6bfe9ce07dcc7a7b4b0f5
Roger Silva <rogersilvati@gmail.com>
2021-10-05 00:37:10 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
+        Seller obj2 = new Seller(1, "Roger Silva", "roger.silva@email.com", new Date(), 3000.00, obj);
         System.out.println(obj);
+        System.out.println(obj2);
     }
 }
